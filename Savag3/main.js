e7a6fd17ce4bf4c2c5fe3fae13a532653fc504bb
diegoesr <83327343+diegoesr@users.noreply.github.com>
2021-04-28 16:28:06 -0500

    Add files via upload

---
+++ Savag3/main.js
@@ -0,0 +1,11 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+  ScrollReveal().reveal('.showcase',{delay:500});
+  ScrollReveal().reveal('.news-cards',{delay:500});
+  ScrollReveal().reveal('.news-cards2',{delay:1000});
+  ScrollReveal().reveal('.cards-banner-one',{delay:500});
+  ScrollReveal().reveal('.cards-banner-two',{delay:500});
+
+
